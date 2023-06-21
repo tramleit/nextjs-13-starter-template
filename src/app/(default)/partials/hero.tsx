6d16f02db15a2a272edf7433f7537094b24654c2
raihanhosen011 <raihanhosen011@gmail.com>
2023-06-22 04:39:32 +0600

    ✨ added : starter template link has been added with github button

--- src/app/(default)/partials/hero.tsx
+++ src/app/(default)/partials/hero.tsx
@@ -28,7 +28,9 @@ export default function Hero() {
 
               <p className='text-sm md:text-xl opacity-75 my-6' > Organized code with NextFuse. Our meticulously crafted starter template for Next.js 13, Tailwind CSS, and TypeScript ensures clean and structured code, enabling you to build scalable and maintainable web applications with utmost efficiency. </p>
 
-              <button className='flex gap-2 bg-white/80 mx-auto text-black px-[10px] py-[10px] items-center rounded-[7px] font-[500] text-sm md:text-base' > <Icon name='github' /> Star on Github </button>
+              <a href='https://github.com/raihanhosen011/nextjs-13-starter-template' target='_blank' rel='norefferer' >
+                <button className='flex gap-2 bg-white/80 mx-auto text-black px-[10px] py-[10px] items-center rounded-[7px] font-[500] text-sm md:text-base' > <Icon name='github' /> Star on Github </button>
+              </a>
 
             </div>
 
